25809f6cec75f24d2a328ac765373c296416c024
natalete <natsalete14@gmail.com>
2025-04-27 15:29:32 -0300

    feat:Funcionalidade de cadastrar carros

--- src/main/java/com/iftm/projeto/carros/controller/CarrosController.java
+++ src/main/java/com/iftm/projeto/carros/controller/CarrosController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import com.iftm.projeto.carros.model.Carros;
 import com.iftm.projeto.carros.service.CarrosService;
+import org.springframework.web.bind.annotation.ModelAttribute;
+ import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class CarrosController {
@@ -19,4 +21,16 @@ public class CarrosController {
         model.addAttribute("carrosList", carrosService.getAllCarros());
         return "carros/index";
     }
+
+    @GetMapping("/carros/create")
+     public String create(Model model) {
+         model.addAttribute("carros", new Carros());
+         return "carros/create";
+     }
+ 
+     @PostMapping("/carros/save")
+     public String postMethodName(@ModelAttribute("carros") Carros carros) {
+         carrosService.saveCarros(carros);
+         return "redirect:/carros";
+     }
 }
